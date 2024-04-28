2631d955fef7bfc38ecf2b3976250fec8f1c5717
OlegatorIT <podlesnyoleg93@gmail.com>
2024-04-28 04:36:04 +0200

    Updates

---
+++ assets/EnglishforEveryone-DtPZQrjc.js
@@ -0,0 +1,22 @@
+import{s as n,B as t,c as s,i,j as e,r as c,b as u,T as p}from"./index-DEMqSzXo.js";import{B as v}from"./Button-CNhmiwqV.js";const x="/LanguageLift/assets/English-for-Everyone.-Level-1-Beginner.-Course-Book.-2016-184p.-Dr3z37iN.pdf",h="/LanguageLift/assets/level1course-DfF1TP1d.jpg",f=n(t)`
+  width: 100px;
+  height: 133px;
+  background-image: url(${h});
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  border-radius: 10px;
+`,g=n(t)`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  gap: 20px;
+  position: relative;
+  /* border: 1px solid ${({theme:r})=>r.palette.divider}; */
+  width: 100%;
+  max-width: 400px;
+  border-radius: 10px;
+`;var a={},j=i;Object.defineProperty(a,"__esModule",{value:!0});var l=a.default=void 0,m=j(s()),_=e;l=a.default=(0,m.default)((0,_.jsx)("path",{d:"M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96M17 13l-5 5-5-5h3V9h4v4z"}),"CloudDownload");var o={},b=i;Object.defineProperty(o,"__esModule",{value:!0});var d=o.default=void 0,y=b(s()),w=e;d=o.default=(0,y.default)((0,w.jsx)("path",{d:"M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2m-8.5 7.5c0 .83-.67 1.5-1.5 1.5H9v2H7.5V7H10c.83 0 1.5.67 1.5 1.5zm5 2c0 .83-.67 1.5-1.5 1.5h-2.5V7H15c.83 0 1.5.67 1.5 1.5zm4-3H19v1h1.5V11H19v2h-1.5V7h3zM9 9.5h1v-1H9zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4zm10 5.5h1v-3h-1z"}),"PictureAsPdf");const D=()=>{const{language:r}=c.useContext(u);return e.jsx(e.Fragment,{children:e.jsxs(g,{children:[e.jsx(t,{children:e.jsx(f,{})}),e.jsxs(t,{sx:{display:"flex",flexDirection:"column",alignItems:"center",gap:1,width:200},children:[e.jsx(p,{variant:"subtitle2",textAlign:"center",children:r==="en"?"English For Everyone Level 1 Course Book":"Підручник з курсу англійської мови для всіх, рівень 1"}),e.jsx("a",{href:x,download:!0,children:e.jsx(v,{variant:"contained",size:"small",startIcon:e.jsx(d,{}),endIcon:e.jsx(l,{}),children:r==="en"?"DownLoad":"Завантажити"})})]})]})})};export{D as default};
