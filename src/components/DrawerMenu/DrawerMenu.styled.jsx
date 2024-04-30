3ab5a82cdb21a9285b2153be299286b613961dae
OlegatorIT <podlesnyoleg93@gmail.com>
2024-04-30 03:52:45 +0200

    Revert "add files"
    This reverts commit 6a8b3ae0748ef68abf7f48cbffaea4ae96269cb6.

--- src/components/DrawerMenu/DrawerMenu.styled.jsx
+++
@@ -1,8 +0,0 @@
-import { styled } from "@mui/material";
-import { NavLink } from "react-router-dom";
-
-export const StyledNavLink = styled(NavLink)`
-  text-decoration: none;
-  color: inherit;
-  width: 100%;
-`;
--- src/components/Footer/Footer.styled.jsx
+++
@@ -1,8 +0,0 @@
-import { Box, styled } from "@mui/material";
-import { motion } from "framer-motion";
-
-export const S_QuoteWrapper = styled(Box)`
-  margin: 15px;
-`;
-
-export const M_QuoteWrapper = motion(S_QuoteWrapper);
